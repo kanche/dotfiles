9c7cde218db71be03c573900190652e8c90f210b
Subhabrata Choudhury <subhabrata@outlook.com>
2015-01-11 15:43:40 +0530

    Little custom settings

--- zsh/my_custom_settings.zsh
+++ zsh/my_custom_settings.zsh
@@ -1,4 +1,5 @@
 export PATH=$(cope_path):$PATH
+export PATH=~/root/.gem/ruby/2.1.0/bin:$PATH
 export PATH=~/stuff/apps/neovim/bin:$PATH
 export WORKON_HOME=~/.virtualenvs
 source /usr/bin/virtualenvwrapper.sh
